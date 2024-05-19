73a35b40ab3a02b89ba0facd12c39b7f25827942
fengkx <liangkx8237@gmail.com>
2024-05-19 21:05:15 +0800

    fix: CI

--- demo/web/rsbuild.config.ts
+++ demo/web/rsbuild.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  output: {
+    assetPrefix: process.env['PUBLIC_URL'] || '/'
+  },
   plugins: [pluginReact()],
 });
