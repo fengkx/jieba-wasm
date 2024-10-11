898ec6b414ba473eb7bcaed74a5fa53a2b2159b3
fengkx <liangkx8237@gmail.com>
2024-10-11 14:00:39 +0800

    feat: add demo page title

--- demo/web/rsbuild.config.ts
+++ demo/web/rsbuild.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
+  html: {
+    title: 'jieba-wasm demo',
+  },
   output: {
     assetPrefix: process.env['PUBLIC_URL'] || '/'
   },
